7485201b41d1e4a7a34901d45ff7c40ef7442e49
aspcodenet <stefan.holmberg@systementor.se>
2024-02-19 09:27:35 +0100

    v2

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const namn = document.getElementById("namn")
 const namnError = document.getElementById("namnError")
+const regno = document.getElementById("regno")
+const regnoError = document.getElementById("regnoError")
+
+
+console.log(validator)
+
+regno.addEventListener("input",()=>{
+    if(validator.isLicensePlate( regno.value, 'sv-SE')){
+        regnoError.style.display="none";
+    }else{
+        regnoError.style.display="block";
+    }
+    
+})
 
 
 namn.addEventListener("input",()=>{
