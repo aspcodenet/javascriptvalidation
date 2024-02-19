b9a41e2ecbe49e18e41ba69b64483c8a2051c3c6
aspcodenet <stefan.holmberg@systementor.se>
2024-02-19 09:14:03 +0100

    v2

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const namn = document.getElementById("namn")
 const namnError = document.getElementById("namnError")
 
 
-namn.addEventListener("blur",()=>{
-    namnError.style.display="none";
+namn.addEventListener("input",()=>{
+    if(namn.value.length > 5){
+        namnError.style.display="none";
+    }else{
+        namnError.style.display="block";
+    }
+    
 })
